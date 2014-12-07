a0029093c1bf8bc2dd41e21cfa464e703714163c
Rodney Hess <rhess@m2xc.com>
2014-12-07 12:20:23 -0600

    Initial Commit

---
+++ DogYears/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DogYears
+//
+//  Created by Rodney Hess on 12/7/14.
+//  Copyright (c) 2014 hess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

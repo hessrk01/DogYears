5c1d99d0e6282192f50227a6f3f724d3ae83593d
Rodney Hess <rhess@m2xc.com>
2014-12-07 23:11:55 -0600

    adding conditional logic

--- DogYears/ViewController.swift
+++ DogYears/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYears: UITextField!
+    @IBOutlet weak var dogYearsConverted: UILabel!
+    @IBOutlet weak var dogYearLabel: UILabel!
+    
+    let dogYearMultiplier = 7
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYears(sender: UIButton) {
+        
+        humanYears.resignFirstResponder()
+        dogYearsConverted.font = UIFont(name: "Zapfino" , size: 30)
+        dogYearsConverted.hidden = false
+        
+        var humanYearsWork = (humanYears.text as NSString).doubleValue
+        var dogYearConversion:Double
+        
+        
+        if humanYearsWork <= 2 {
+            dogYearConversion = humanYearsWork * 10.5
+        }
+        else {
+            dogYearConversion = (21) + ((humanYearsWork - 2)*4)
+            
+        }
+        
+        dogYearsConverted.text = "\(dogYearConversion)"
+        dogYearLabel.hidden = false
+        humanYears.text = ""
+    }
 
 }
 
